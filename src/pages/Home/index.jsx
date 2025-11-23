3ea02979949dc36b0560f2abc5f30e7bac8d6ab2
Corentin Nicolas <c.nicolas28210@gmail.com>
2025-11-23 17:43:07 +0100

    feat(banner): add banner component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,12 @@
+import homeBanner from "../../assets/images/banner-home.webp";
+import Banner from "../../components/Banner";
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <Banner image={homeBanner} title="Chez vous, partout et ailleurs" />
+    </>
+  );
 }
 
 export default Home;
